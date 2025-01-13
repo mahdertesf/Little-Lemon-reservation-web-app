148dac02c0698e24aed7ef5b85d5b04ec093c948
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-13 09:29:11 +0300

    add restaurant, rest_framework, and corsheaders to installed apps

--- backend/littlelemon/settings.py
+++ backend/littlelemon/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
+    'rest_framework',
+    'corsheaders',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
