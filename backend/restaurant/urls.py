009fc335ce589c1d383fd0f96dfb91123c260d80
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 15:33:34 +0300

    fix: add missing newline at the end of urls.py

--- backend/restaurant/urls.py
+++ backend/restaurant/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('menu/',MenuItemView.as_view(),name='menu'),
     path('menu/<int:pk>/',SingleMenuItemView.as_view(),name='single-menu'),
     path('api-token-auth/', obtain_auth_token, name='api_token-auth'),
+    
    
 ]
