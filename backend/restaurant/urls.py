73ae9684683c20fb2a28266b9f0bb88f0c033ae5
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 14:57:30 +0300

    feat: add API token authentication endpoint to URLs

--- backend/restaurant/urls.py
+++ backend/restaurant/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path , include
 from .views import MenuItemView,SingleMenuItemView , BookingViewSet
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu/',MenuItemView.as_view(),name='menu'),
     path('menu/<int:pk>/',SingleMenuItemView.as_view(),name='single-menu'),
+    path('api-token-auth/', obtain_auth_token, name='api_token-auth'),
    
 ]
