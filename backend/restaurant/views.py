6a6b0b0a633950a3501657493df2651581fa5211
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 12:20:22 +0300

    add API endpoints for menu items and create serializers

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Menu,Booking
+from .serializers import MenuSerializer
 # Create your views here.
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+    
