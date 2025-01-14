fda68ee3a0f5e40eaf88f5c11a0aa0cc650e5dd9
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 10:49:25 +0300

    configure MySQL database settings and add mysqlclient dependency

--- backend/littlelemon/settings.py
+++ backend/littlelemon/settings.py
@@ -87,9 +87,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'USER':'backend_capstone',
+        'PASSWORD':'3346Glu??'
+    },
+    
 }
 
 
