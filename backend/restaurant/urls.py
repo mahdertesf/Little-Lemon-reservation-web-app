691ca26e933e629251cb092e1f7a291f2af4bdc0
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 12:41:55 +0300

    add booking API endpoints using DefaultRouter for table reservations

--- backend/restaurant/urls.py
+++ backend/restaurant/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import MenuItemView,SingleMenuItemView
+from django.urls import path , include
+from .views import MenuItemView,SingleMenuItemView , BookingViewSet
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+router.register(r'tables',BookingViewSet)
 urlpatterns = [
     path('menu/',MenuItemView.as_view(),name='menu'),
     path('menu/<int:pk>/',SingleMenuItemView.as_view(),name='single-menu'),
+    path('restaurant/booking/',include(router.urls)),
 ]
