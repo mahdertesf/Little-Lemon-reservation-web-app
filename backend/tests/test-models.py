f76c899e67e17ad691e90a512f07dec02539f30c
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 19:07:36 +0300

    refactor: remove obsolete unit test for Menu model

--- backend/tests/test-models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from restaurant.models import Menu
-
-class MenuTest(TestCase):
-    def test_get_item(self):
-        item=Menu.objects.create(title='Pizza',price=10.00,inventory=10)
-        self.assertEqual(item,'Pizza : 10.00')
